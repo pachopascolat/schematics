7e60a3faf85ecd5dea42e5045d46fbf36e58b6b6
pachopascolat <patriciopascolat@gmail.com>
2022-10-22 22:50:45 -0300

    added typeOrm wired

--- src/lib/resource/files/ts/__name__.service.ts
+++ src/lib/resource/files/ts/__name__.service.ts
@@ -1,28 +1,46 @@
+
 import { Injectable } from '@nestjs/common';<% if (crud && type !== 'graphql-code-first' && type !== 'graphql-schema-first') { %>
-import { Create<%= singular(classify(name)) %>Dto } from './dto/create-<%= singular(name) %>.dto';
-import { Update<%= singular(classify(name)) %>Dto } from './dto/update-<%= singular(name) %>.dto';<% } else if (crud) { %>
-import { Create<%= singular(classify(name)) %>Input } from './dto/create-<%= singular(name) %>.input';
-import { Update<%= singular(classify(name)) %>Input } from './dto/update-<%= singular(name) %>.input';<% } %>
+  import { Create<%= singular(classify(name)) %>Dto } from './dto/create-<%= singular(name) %>.dto';
+  import { Update<%= singular(classify(name)) %>Dto } from './dto/update-<%= singular(name) %>.dto';
+  import {<%= singular(classify(name)) %>} from './entities/<%= singular(name) %>.entity';
+  <% } else if (crud) { %>
+  import { Create<%= singular(classify(name)) %>Input } from './dto/create-<%= singular(name) %>.input';
+  import { Update<%= singular(classify(name)) %>Input } from './dto/update-<%= singular(name) %>.input';
+  import {<%= singular(classify(name)) %>} from './entities/<%= singular(name) %>.entity';
+
+  <% } %>
+import {Repository} from "typeorm";
+import {InjectRepository} from "@nestjs/typeorm";
+
+
 
 @Injectable()
 export class <%= classify(name) %>Service {<% if (crud) { %>
-  create(<% if (type !== 'graphql-code-first' && type !== 'graphql-schema-first') { %>create<%= singular(classify(name)) %>Dto: Create<%= singular(classify(name)) %>Dto<% } else { %>create<%= singular(classify(name)) %>Input: Create<%= singular(classify(name)) %>Input<% } %>) {
-    return 'This action adds a new <%= lowercased(singular(classify(name))) %>';
+
+
+  constructor(
+    @InjectRepository(<%= singular(classify(name)) %>)
+private readonly <%= lowercased(singular(classify(name))) %>Repository: Repository<<%= singular(classify(name)) %>>,
+) {}
+
+
+  async create(<% if (type !== 'graphql-code-first' && type !== 'graphql-schema-first') { %>create<%= singular(classify(name)) %>Dto: Create<%= singular(classify(name)) %>Dto<% } else { %>create<%= singular(classify(name)) %>Input: Create<%= singular(classify(name)) %>Input<% } %>) {
+    return await this.<%= lowercased(singular(classify(name))) %>Repository.save(create<%= singular(classify(name)) %>Dto);
   }
 
-  findAll() {
-    return `This action returns all <%= lowercased(classify(name)) %>`;
+  async findAll() {
+    return await this.<%= lowercased(singular(classify(name))) %>Repository.find();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} <%= lowercased(singular(classify(name))) %>`;
+  async findOne(id: number) {
+    return await this.<%= lowercased(singular(classify(name))) %>Repository.findOneBy({id:id});
   }
 
-  update(id: number, <% if (type !== 'graphql-code-first' && type !== 'graphql-schema-first') { %>update<%= singular(classify(name)) %>Dto: Update<%= singular(classify(name)) %>Dto<% } else { %>update<%= singular(classify(name)) %>Input: Update<%= singular(classify(name)) %>Input<% } %>) {
-    return `This action updates a #${id} <%= lowercased(singular(classify(name))) %>`;
+  async update(id: number, <% if (type !== 'graphql-code-first' && type !== 'graphql-schema-first') { %>update<%= singular(classify(name)) %>Dto: Update<%= singular(classify(name)) %>Dto<% } else { %>update<%= singular(classify(name)) %>Input: Update<%= singular(classify(name)) %>Input<% } %>) {
+    return await this.<%= lowercased(singular(classify(name))) %>Repository.update(id,update<%= singular(classify(name)) %>Dto);
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} <%= lowercased(singular(classify(name))) %>`;
+  async remove(id: number) {
+    return await this.<%= lowercased(singular(classify(name))) %>Repository.delete(id);
   }
-<% } %>}
+  <% } %>}
--- src/lib/resource/files/ts/entities/__name@singular@ent__.ts
+++ src/lib/resource/files/ts/entities/__name@singular@ent__.ts
@@ -1,7 +1,11 @@
 <% if (type === 'graphql-code-first') { %>import { ObjectType, Field, Int } from '@nestjs/graphql';
 
-@ObjectType()
-export class <%= singular(classify(name)) %> {
-  @Field(() => Int, { description: 'Example field (placeholder)' })
-  exampleField: number;
-}<% } else { %>export class <%= singular(classify(name)) %> {}<% } %>
+  @ObjectType()
+  export class <%= singular(classify(name)) %> {
+    @Field(() => Int, { description: 'Example field (placeholder)' })
+    exampleField: number;
+  }<% } else { %>
+  import {Entity} from 'typeorm';
+
+  @Entity()
+  export class <%= singular(classify(name)) %> {}<% } %>
